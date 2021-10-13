1484be57a37840ab5c1c5453cff7e00efe5cae97
aliyawer <aliyawerqambari@gmail.com>
2021-10-13 12:07:23 +0200

    Add a celery worker

--- api/app.py
+++ api/app.py
@@ -1,15 +1,22 @@
 #!flask/bin/python
-from flask import Flask, jsonify
-import subprocess
-import sys
+from flask import Flask
+from celery import Celery
 
 app = Flask(__name__)
 
+celery = Celery('worker',
+                broker="amqp://admin:admin@rabbit",
+                backend="rpc://")
 
 @app.route('/baas', methods=['GET'])
 def baas_project():
-    return "Running BaaS project!!!!"
+    result = benchop.delay()
+    return result.get()
 
+
+@celery.task(name="app.benchop")
+def benchop():
+    return "Benchop task is running"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
