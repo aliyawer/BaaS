bef1b525d160b1559fe81f1346fd50fa0a1e5878
lovvvan <lovisa.hedman93@gmail.com>
2021-10-21 17:43:11 +0200

    added another task

--- api/app.py
+++ api/app.py
@@ -13,29 +13,48 @@ celery = Celery('worker',
 
 @app.route('/baas', methods=['GET'])
 def baas_project():
-    result = benchop.delay()
-    return result.get()
+    res_1 = task_1.delay()
+    res_2 = task_2.delay()
+    res = {
+        "res_1": res_1.get(),
+        "res_2": res_2.get(),
+    }
+    return res
+
+def flatten_list(the_list):
+    flattened_list = []
+    for sublist in the_list:
+        for subsublist in sublist:
+            flattened_list.append(subsublist)
+    return flattened_list
 
 
-@celery.task(name="app.benchop")
-def benchop():
+@celery.task(name="app.task_1")
+def task_1():
     oc = Oct2Py()
     oc.addpath('./BENCHOP')
     time, res = oc.Table_1a(nout=2)
-    flat_time = []
-    for time_list in time:
-        for sublist in time_list:
-            flat_time.append(sublist)
-    flat_res = []
-    for r in res:
-        for el in r:
-            flat_res.append(el)
+    flat_time = flatten_list(time)
+    flat_res = flatten_list(res)
 
     result_dict = {
         "flat_time": flat_time,
         "flat_res": flat_res
     }
+    return result_dict
 
+@celery.task(name="app.task_2")
+def task_2():
+    oc = Oct2Py()
+    oc.addpath('./BENCHOP')
+    time, res = oc.Table_1b1(nout=2)
+    flat_time = flatten_list(time)
+    flat_res = flatten_list(res)
+
+    result_dict = {
+        "flat_time": flat_time,
+        "flat_res": flat_res
+    }
     return result_dict
 
 
